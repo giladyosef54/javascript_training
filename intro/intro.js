7df118d84c3d3ff41dace43f45fb5c8d1e386118
giladyosef54 <57717590+giladyosef54@users.noreply.github.com>
2024-02-04 17:24:47 +0200

    intro 1

--- intro/intro.js
+++ intro/intro.js
@@ -1,1 +1,23 @@
-console.log("Hello world.")
+function createFiles(filesData){
+    const fs = require('fs')
+     
+    for (let i in filesData) {
+        fs.writeFile(filesData[i].fileName + '.' + filesData[i].fileType,
+                     filesData[i].fileData.toString(),
+                     (err) => {
+                        if (err)
+                            console.log(err)
+                        else console.log('success!')
+                     })
+    }
+}
+
+
+fileData = {
+    fileName:"number",
+    fileType:'txt',
+    fileData:122
+}
+
+l = [fileData]
+createFiles(l)
