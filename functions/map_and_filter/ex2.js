6b109ff7d3689cf69f5754a450a92c8f75047e71
giladyosef54 <57717590+giladyosef54@users.noreply.github.com>
2024-02-05 16:59:59 +0200

    finish map_and_filter ex2

---
+++ functions/map_and_filter/ex2.js
@@ -0,0 +1,8 @@
+function reduceHours(arrOfDurations) {
+    return arrOfDurations.filter(duration => {
+        return duration >= 24
+    }).map(longDuration => {
+        return longDuration/24
+    })
+}
+
