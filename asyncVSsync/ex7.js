f4a9bab2d3b5c5866c363502c09d0aeb5bddd64d
giladyosef54 <57717590+giladyosef54@users.noreply.github.com>
2024-02-07 18:45:33 +0200

    finish ex7

---
+++ asyncVSsync/ex7.js
@@ -0,0 +1,17 @@
+function createNestedPromise() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(new Promise((resolve) => {
+                setTimeout(() => {
+                    utl = require('./utilities')
+                    const randInt = utl.getRndInteger(1, 10)
+                    resolve(randInt)
+                }, 1000)
+            }))
+        }, 500)
+        
+    })
+}
+
+
+
