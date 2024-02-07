fc9c31a4fbf315d425ea0c52ddc9fc47fcb8e8fc
giladyosef54 <57717590+giladyosef54@users.noreply.github.com>
2024-02-07 17:00:27 +0200

    finish ex4

---
+++ asyncVSsync/ex4.js
@@ -0,0 +1,19 @@
+function promiseForeachNumber(num) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(num);
+        }, num * 500);
+    });
+}
+
+
+  
+function printSortedArray(arr) {
+    for (let num of arr) {
+        promiseForeachNumber(num).then(num => console.log(num));
+    }
+    
+}
+
+
+
