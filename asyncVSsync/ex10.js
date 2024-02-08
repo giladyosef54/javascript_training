0090634ef6076b5d88651eec6317ff0d49605b8d
giladyosef54 <57717590+giladyosef54@users.noreply.github.com>
2024-02-08 11:19:38 +0200

    delete unnecessary line

--- asyncVSsync/ex10.js
+++ asyncVSsync/ex10.js
@@ -1,6 +1,3 @@
 function isBothGood(promise1, promise2) {
     return Promise.all([promise1, promise2])
 }
-
-
-isBothGood(Promise.reject('first'), Promise.resolve('second')).then(values => console.log(values)).catch(err => console.error(err))
