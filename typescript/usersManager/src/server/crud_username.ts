fa98d113579e327e08c2babd2e42f49708fed9a4
giladyosef54 <57717590+giladyosef54@users.noreply.github.com>
2024-02-12 17:05:12 +0200

    update server imp

--- typescript/usersManager/src/server/crud_username.ts
+++ typescript/usersManager/src/server/crud_username.ts
@@ -2,7 +2,7 @@ import { Router, Request, Response } from 'express';
 import { Username } from '../models/username';
 
 const router = Router();
-let usernames: Username[] = [];
+let users: Username[] = [];
 
 router.post('/saveUsersData', (req: Request, res: Response) => {
     for (let userData of req.body.users) {
@@ -10,10 +10,23 @@ router.post('/saveUsersData', (req: Request, res: Response) => {
             username: userData.username,
             password: userData.password
         };
-        usernames.push(username);
+        users.push(username);
     }
     res.status(201).send('successfully saved');
 });
-  
+
+
+router.get('/getUserDetails/:username', (req: Request, res: Response) => {
+    const user = users.find((user => user.username == req.params.username))
+    if (user) res.status(200).json(user)
+    else res.status(404).send('username not found')
+})
+
+
+const un: Username = {
+    username: 'josef',
+    password: '31'
+};
+users.push(un);
 
 export default router;
