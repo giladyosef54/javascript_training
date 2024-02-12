51f161575d6e631d10943a88dc30a53e93c96a73
giladyosef54 <57717590+giladyosef54@users.noreply.github.com>
2024-02-12 19:28:07 +0200

    creating client operations

--- typescript/usersManager/src/client/client_requests.ts
+++ typescript/usersManager/src/client/client_requests.ts
@@ -1,3 +1,15 @@
-const ensureUserExistence = () => {
-    
+import axios, { AxiosResponse } from 'axios'
+import { Username } from '../models/username'
+
+const ensureUserExistence = (username: string, password: string, baseUrl: string = 'http://localhost:3000/',
+        getUserRoute: string = 'getUserDetails/', saveUserRoute: string = 'saveUsersData') => {
+    axios.get(baseUrl + getUserRoute + username).then((res: AxiosResponse) => {
+        if (res.status == 404) {
+            const newUser: Username = {
+                username: username,
+                password: password
+            }
+            axios.post(baseUrl + saveUserRoute, newUser)
+        }
+    })
 }
