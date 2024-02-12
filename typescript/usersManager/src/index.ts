9bf6be3f977eeaaf7545d6734c20435122b40841
giladyosef54 <57717590+giladyosef54@users.noreply.github.com>
2024-02-12 14:04:12 +0200

    delete unnecessary lines

--- typescript/usersManager/src/index.ts
+++ typescript/usersManager/src/index.ts
@@ -8,10 +8,6 @@ app.use(express.json())
 app.use('/', router)
 
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, TypeScript Express!');
-});
-
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
