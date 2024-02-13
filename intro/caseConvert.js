341d3f84ac0a61927d33765820c3cd33cee38ed5
giladyosef54 <57717590+giladyosef54@users.noreply.github.com>
2024-02-13 16:57:45 +0200

    init utilities and update caseConvert

--- intro/caseConvert.js
+++ intro/caseConvert.js
@@ -30,20 +30,20 @@ const converter = {
 
 const fixStreetName = (filename) => {
     fs.readFile(filename, (err, data) => {
-        if (err) console.log(err)
+        if (err) logger.error(err)
         else {
-            console.log('Successfully read.')
+            logger.info('Successfully read.')
             data = data.toString().trim().split('\n')
             fixData = ''
-            for (let line of data)
+            for (const line of data)
             {
                 let splitedLine = line.split('|')
                 let lineCase = splitedLine[0], lineData = splitedLine[1]
                 fixData += converter[lineCase](lineData.split('_')) + '\n'
             }
             fs.writeFile('results.txt', fixData, (err) => {
-                if (err) console.log(err)
-                else console.log('Successfully written.')})
+                if (err) logger.error(err)
+                else logger.info('Successfully written.')})
 
         }
     });
