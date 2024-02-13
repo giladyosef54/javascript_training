b374101f0e097839f031e42eef05718a15059038
giladyosef54 <57717590+giladyosef54@users.noreply.github.com>
2024-02-13 16:22:03 +0200

    update ex7

--- asyncVSsync/ex7.js
+++ asyncVSsync/ex7.js
@@ -1,9 +1,10 @@
-function createNestedPromise() {
+utl = require('./utilities')
+
+const createNestedPromise = () => {
     return new Promise((resolve) => {
         setTimeout(() => {
             resolve(new Promise((resolve) => {
                 setTimeout(() => {
-                    utl = require('./utilities')
                     const randInt = utl.getRndInteger(1, 10)
                     resolve(randInt)
                 }, 1000)
