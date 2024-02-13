b25f8edf0f0ec1516744d8165ad21986b85d9e1c
giladyosef54 <57717590+giladyosef54@users.noreply.github.com>
2024-02-13 16:32:21 +0200

    update ex11

--- asyncVSsync/ex12.js
+++ asyncVSsync/ex12.js
@@ -1,10 +1,12 @@
-function getSmaller(arrOfNum) {
+const logger = require('./utilities').logger
+
+const getSmaller = (arrOfNum) => {
     const promisesPerNum = []
     for (num of arrOfNum) {
         promisesPerNum.push(new Promise((resolve) => {
             setTimeout(resolve, num * 500, num)
         }))
     }
-    Promise.race(promisesPerNum).then(num => console.log(num))
+    Promise.race(promisesPerNum).then(num => logger.info(num))
 }
 
