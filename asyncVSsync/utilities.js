ea9bad445b1e9dacf88b7cc7e176db0c75f18a6d
giladyosef54 <57717590+giladyosef54@users.noreply.github.com>
2024-02-13 15:41:59 +0200

    add logger to utilities

--- asyncVSsync/utilities.js
+++ asyncVSsync/utilities.js
@@ -1,5 +1,11 @@
+const winston = require('winston')
+const logger = winston.createLogger({
+    level: 'info',
+    transports: [new winston.transports.Console()]
+})
+
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-module.exports = { getRndInteger };
+module.exports = { getRndInteger, logger };
