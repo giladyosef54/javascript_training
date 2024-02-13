399d9fbe28d645ceb690f75867ff68d9254ae826
giladyosef54 <57717590+giladyosef54@users.noreply.github.com>
2024-02-13 16:33:03 +0200

    update ex11

--- asyncVSsync/ex13.js
+++ asyncVSsync/ex13.js
@@ -1,13 +1,15 @@
-function repeatArrOnScreen(arrOfNums) {
+const logger = require('./utilities').logger
+
+
+const repeatArrOnScreen = (arrOfNums) => {
     for (num of arrOfNums) {
         new Promise((resolve) => {
             setTimeout((inervalId) => {
                 clearInterval(inervalId)
                 resolve()
-            }, 5000, setInterval(console.log, 1500, num))
+            }, 5000, setInterval(logger.info, 1500, num))
         })
     }
 }
 
 
-repeatArrOnScreen([1,3])//,3443,34,5432,524])
