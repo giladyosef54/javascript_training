343ddcf1e4f1db495d3b9f569983001f60715cb6
giladyosef54 <57717590+giladyosef54@users.noreply.github.com>
2024-02-13 16:15:46 +0200

    update ex4

--- asyncVSsync/ex5.js
+++ asyncVSsync/ex5.js
@@ -1,14 +1,15 @@
-function createRandomPromise() {
+const utl = require('./utilities')
+
+const createRandomPromise = () => {
     return new Promise((reserve) => {
-        const utl = require('./utilities')
         setTimeout(() => {
             reserve()
         }, utl.getRndInteger(0, 20) * 1000) 
     })
 }
 
-function createSomePromises(num) {
+const createSomePromises = (num) => {
     for (let i = 0; i < num; ++i) {
-        createRandomPromise().then(() => console.log(`promise ${i} end`))
+        createRandomPromise().then(() => utl.logger.info(`promise ${i} end`))
     }
 }
