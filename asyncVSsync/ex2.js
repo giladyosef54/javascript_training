3fedcaa0372dbe9332c528afaded53f2f338d680
giladyosef54 <57717590+giladyosef54@users.noreply.github.com>
2024-02-13 15:44:44 +0200

    update ex2

--- asyncVSsync/ex2.js
+++ asyncVSsync/ex2.js
@@ -1,7 +1,9 @@
-function retrunRndIn3Sec() {
+const utl = require('./utilities')
+
+const retrunRndIn3Sec = () => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            const utl = require('./utilities')
+            
             const randInt = utl.getRndInteger(50, 100)
             if (randInt > 85) reject('Too big number')
             else resolve(randInt);
@@ -9,6 +11,7 @@ function retrunRndIn3Sec() {
     });
 }
   
-function printValidInt() {
-    retrunRndIn3Sec().then(result => console.log(result)).catch(error => console.error(error));
+const printValidInt = () => {
+    retrunRndIn3Sec().then(result => utl.logger.info(result)).catch(error => utl.logger.error(error));
 }
+
