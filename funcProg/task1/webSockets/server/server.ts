2d21811602762306b52e4e979f1c9d73a748dfa5
giladyosef54 <57717590+giladyosef54@users.noreply.github.com>
2024-02-14 09:56:50 +0200

    update server.ts

--- funcProg/task1/webSockets/server/server.ts
+++ funcProg/task1/webSockets/server/server.ts
@@ -5,4 +5,12 @@ const port = 1234;
 const wss = new WebSocketServer({ port })
 
 
+wss.on('connection', (wsc) => {
+    wsc.on('massage', (data) => {
+        serverLogger.info(`Recieved mssaga from client: ${data}`)
+    })
+
+    wsc.send('Hello, this is server')
+})
+
 serverLogger.info('WebSocket server.')
