1c9841ae59c212a62859be9b5d0457e29903264b
giladyosef54 <57717590+giladyosef54@users.noreply.github.com>
2024-02-15 13:45:25 +0200

    Finish task1

--- funcProg/task1/webSockets/client/client.ts
+++ funcProg/task1/webSockets/client/client.ts
@@ -1,17 +1,47 @@
 import { WebSocket } from "ws";
-import { logger } from "../utilities/utilities";
+import { logger, getRndInt } from "../utilities/utilities";
 
 const port = 1234
-const ws = new WebSocket(`ws://localhost:${port}`)
-const clientGuess: number
+const min: number = 0, max: number = 3
 
-ws.on('open', () => {
+
+class KeyWebSocket extends WebSocket {
+    key: number|undefined = undefined
+}
+
+
+const kws = new KeyWebSocket(`ws://localhost:${port}`)
+
+
+kws.on('open', () => {
     logger.info('Client connected.')
-    clientGuess = 
-    ws.send('Hello this is a client, please start guesting')
+    kws.key = getRndInt(min, max)
+    kws.send(JSON.stringify({
+        message: `Hello this is a client, please guess a number between ${min} to ${max}`,
+        messageType: 'initGame',
+        min: min,
+        max: max
+    }))
+    
 })
 
-ws.on('message', (data) => {
-    if (data == )
-    logger.info(`Recieved message from the server: ${data}`)
+
+kws.on('message', (data) => {
+    const parsedData = JSON.parse(data.toString())
+    logger.info(`Recieved message from the server: ${parsedData.message}`)
+    if (parsedData.isGuessing === true) {
+        if (parsedData.serverGuess === kws.key)
+        {
+            kws.send(JSON.stringify({
+                message: `You won the game!`,
+                messageType: 'hit'
+            }))
+        }
+        else {
+            kws.send(JSON.stringify({
+                message: `Failed guessing, try again.`,
+                messageType: 'miss'
+            }))
+        }
+    }  
 })
--- funcProg/task1/webSockets/server/server.ts
+++ funcProg/task1/webSockets/server/server.ts
@@ -6,11 +6,37 @@ const wss = new WebSocketServer({ port })
 
 
 wss.on('connection', (ws) => {
-    ws.on('massage', (data) => {
-        logger.info(`Recieved mssaga from client: ${data}`)
-    })
+    ws.on('message', (data) => {
+        const parsedData = JSON.parse(data.toString())
+        logger.info(`Recieved message from client: ${parsedData.message}`)
+        
+        if (parsedData.messageType === 'initGame')
+        {
+            
+            const tryAcomplish = () => {
+                const serverGuess = getRndInt(parsedData.min, parsedData.max)
+                ws.send(JSON.stringify({
+                    message: `Tried to guess the number ${serverGuess}, did I succeed?`,
+                    serverGuess: serverGuess,
+                    isGuessing: true
+                }))
+            }
+            const remindExistence = () => {
+                ws.send(JSON.stringify({
+                    message: 'Calculating next guess...',
+                    isGuessing: false
+                }))
+            }
 
-    ws.send('Hello, this is server')
+            setInterval(tryAcomplish, 2000)
+            setTimeout(setInterval, 1000, remindExistence, 2000)
+            
+        }
+        else if (parsedData.messageType === 'hit')
+        {
+            ws.terminate()
+        }
+    })
 })
 
-logger.info('WebSocket server.')
+
