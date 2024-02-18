c690183303499fcce627b287674cac035e38e127
giladyosef54 <57717590+giladyosef54@users.noreply.github.com>
2024-02-18 13:10:30 +0200

    init utilities

--- funcProg/task2/client/client.ts
+++ funcProg/task2/client/client.ts
@@ -2,3 +2,13 @@ import { WebSocket } from "ws";
 import {fileStructure} from '../models/types'
 import dotenv from 'dotenv';
 dotenv.config();
+
+const ip = process.env.IP
+const port = process.env.PORT 
+
+
+const ws = new WebSocket(`ws://${ip}:${port}`)
+
+ws.on('open', () => {
+    
+})
--- funcProg/task2/server/server.ts
+++ funcProg/task2/server/server.ts
@@ -1,5 +1,6 @@
 import { WebSocketServer } from "ws";
 import {fileStructure} from '../models/types'
+import { logger } from "../utilities/utilities";
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -9,7 +10,9 @@ const port = +(process.env.PORT || '3000')
 const wss = new WebSocketServer({ port })
 
 wss.on('connection', ws=> {
+    logger.info(`Server connected to client`)
     ws.on('message', data => {
+        logger.info(`Recieving message from client: ${data}`)
         const newFile: fileStructure = JSON.parse(data.toString())
         const fileIndex = files.findIndex(file => { file.fileName == newFile.fileName && file.fileType == newFile.fileType})
         const createFile = (newFile: fileStructure) => {
