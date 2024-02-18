7e5676f5002d84f992f51eb5736dcdeb28bc04ae
giladyosef54 <57717590+giladyosef54@users.noreply.github.com>
2024-02-18 10:46:06 +0200

    implement server message event

--- funcProg/task2/server/server.ts
+++ funcProg/task2/server/server.ts
@@ -1,8 +1,32 @@
 import { WebSocketServer } from "ws";
+import {fileStructure} from '../models/types'
+import dotenv from 'dotenv';
+dotenv.config();
 
-const port = 1234;
+const files: fileStructure[] = []
+
+const port = +(process.env.PORT || '3000')
 const wss = new WebSocketServer({ port })
 
 wss.on('connection', ws=> {
-    
+    ws.on('message', data => {
+        const newFile: fileStructure = JSON.parse(data.toString())
+        const fileIndex = files.findIndex(file => { file.fileName == newFile.fileName && file.fileType == newFile.fileType})
+        const createFile = (newFile: fileStructure) => {
+            files.push(newFile)
+            ws.send('File already exist, data modified.')
+        }
+        const modifyFile = (fileIndex: number) => {
+            files[fileIndex].fileData = newFile.fileData
+            ws.send('File created.')
+        
+        }
+
+        if (fileIndex == -1) {
+            createFile(newFile)
+        }
+        else {
+            modifyFile(fileIndex)
+        }
+    })
 })
