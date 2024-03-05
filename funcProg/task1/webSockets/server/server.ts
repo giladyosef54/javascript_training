9c0ee98e9c41e1b4d171db11dedbe0e569a261e1
giladyosef54 <57717590+giladyosef54@users.noreply.github.com>
2024-03-05 10:14:56 +0200

    move functions to global space

--- funcProg/task1/webSockets/server/server.ts
+++ funcProg/task1/webSockets/server/server.ts
@@ -1,8 +1,24 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 import { logger, getRndInt } from "../utilities/utilities";
 
 const port = 1234;
 const wss = new WebSocketServer({ port })
+
+const tryAcomplish = (ws: WebSocket, min: number, max: number) => {
+    const serverGuess = getRndInt(min, max)
+    ws.send(JSON.stringify({
+        message: `Tried to guess the number ${serverGuess}, did I succeed?`,
+        serverGuess: serverGuess,
+        isGuessing: true
+    }))
+}
+
+const remindExistence = (ws: WebSocket) => {
+    ws.send(JSON.stringify({
+        message: 'Calculating next guess...',
+        isGuessing: false
+    }))
+}
 
 
 wss.on('connection', (ws) => {
@@ -37,6 +53,15 @@ wss.on('connection', (ws) => {
             ws.terminate()
         }
     })
-})
 
+    ws.on('initeGame', (min: number, max: number) => {
+
+        setInterval(tryAcomplish, 2000, ws, min, max)
+        setTimeout(setInterval, 1000, remindExistence, 2000, ws)
+    })
+
+    ws.on('hit', () => {
+        ws.terminate()
+    })
+})
 
