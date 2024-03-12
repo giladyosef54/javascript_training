84ddf2e8c3c1f9a31185590609a89394673fcd56
giladyosef54 <57717590+giladyosef54@users.noreply.github.com>
2024-03-12 17:14:51 +0200

    modify export according to PR

--- funcProg/task3/models/types.ts
+++ funcProg/task3/models/types.ts
@@ -1,20 +1,19 @@
 import { WebSocket } from 'ws'
 
 
-interface Message {
+export interface Message {
     text: string
 }
 
-interface DstMessage {
+export interface DstMessage {
     dst: string,
     text: string
 }
 
-interface wsDetails {
+export interface wsDetails {
     id: number,
     name: string,
     ws: WebSocket
 }
 
 
-export { Message, DstMessage, wsDetails }
