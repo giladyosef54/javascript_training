425ef3cc23f991f451edc3fe4e7a82a2be7657a0
giladyosef54 <57717590+giladyosef54@users.noreply.github.com>
2024-03-12 15:13:22 +0200

    delete unnecessary file

--- funcProg/task1/webSockets/utilities/utilities.ts
+++ funcProg/task1/webSockets/utilities/utilities.ts
@@ -13,7 +13,7 @@ const logger = createLogger({
 });
 
 
-function getRndInt(min: number, max: number) {
+const getRndInt = (min: number, max: number) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
   
