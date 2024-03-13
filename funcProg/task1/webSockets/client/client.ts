7db0b03d3087e6acf1875ce207304067ed372780
giladyosef54 <57717590+giladyosef54@users.noreply.github.com>
2024-03-13 09:03:57 +0200

    change client infos

--- funcProg/task1/webSockets/client/client.ts
+++ funcProg/task1/webSockets/client/client.ts
@@ -1,6 +1,7 @@
 import { WebSocket } from "ws";
 import { logger, getRndInt } from "../utilities/utilities";
 
+
 const port = 1234
 const min: number = 0, max: number = 100
 
@@ -16,9 +17,10 @@ const kws = new KeyWebSocket(`ws://localhost:${port}`)
 kws.on('open', () => {
     logger.info('Client connected.')
     kws.key = getRndInt(min, max)
+    logger.info(`Starting a game with server. Let's see how much time it takes it to guess my number. My number is ${kws.key}.`)
     kws.send(JSON.stringify({
         eventName: 'initGame',
-        message: `Hello this is a client, please guess a number between ${min} to ${max}`,
+        message: `Hello this is a client, please guess a number between ${min} to ${max}.`,
         min: min,
         max: max
     }))
@@ -28,7 +30,7 @@ kws.on('open', () => {
 
 kws.on('message', (data) => {
     const {eventName, ...eventData} = JSON.parse(data.toString())
-
+    logger.info(`Some kind of data recieved from server.`)
     if (kws.listeners(eventName).length != 0) {
         try {
             kws.emit(eventName, eventData)
@@ -37,18 +39,26 @@ kws.on('message', (data) => {
             logger.error((error as TypeError).message)
         }
     }
+    else logger.info(`This kind of data is not supported by my events handlers. this is the raw data we have got:
+Event name: ${eventName}
+Event data: ${eventData}`)
 })
 
 kws.on('guess', ({serverGuess, message}) => {
     logger.info(`Recieved guess request from server: ${message}`)
     if (serverGuess === kws.key)
     {
+        logger.info(`The server guess (${serverGuess}) was succesful! Now sending the happy news to server.`)
         kws.send(JSON.stringify({
             eventName: 'hit',
             message: `${serverGuess} was successful! You won the game!!!`
         }))
+        logger.info(`Terminate myself. Goodbye!`)
+        kws.terminate()
+
     }
     else {
+        logger.info(`Unfotunately the server guess (${serverGuess}) was failed. Now sending the result to server.`)
         kws.send(JSON.stringify({
             eventName: 'miss',
             message: `${serverGuess} was failed guess, try again.`,
@@ -57,7 +67,7 @@ kws.on('guess', ({serverGuess, message}) => {
 })
 
 kws.on('infoClient', ({message}) => {
-    logger.info(`Recieved info from server: ${message}`)
+    logger.info(`Recieved an info from server: ${message}`)
 })
 
 kws.on('eventNameError', ({message}) => {
